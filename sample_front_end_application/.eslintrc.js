75205c21475cdaa095e806926fcfd69eb3cd3fbb
橋本 祐介 <yhashimoto@nextscape.net>
2022-06-10 11:59:59 +0900

    eslint修正

--- sample_front_end_application/.eslintrc.js
+++ sample_front_end_application/.eslintrc.js
@@ -4,11 +4,16 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
   },
   rules: {
